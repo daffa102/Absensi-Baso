e153cd11925eeda9b1e2470f023f27c3b74f44bc
rafif nur ahmad <rapipna19@gmail.com>
2026-01-06 21:34:24 +0700

    add components

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Login extends Component
 {
+    #[Layout('components.layouts.app')]
     public function render()
     {
         return view('livewire.auth.login');
